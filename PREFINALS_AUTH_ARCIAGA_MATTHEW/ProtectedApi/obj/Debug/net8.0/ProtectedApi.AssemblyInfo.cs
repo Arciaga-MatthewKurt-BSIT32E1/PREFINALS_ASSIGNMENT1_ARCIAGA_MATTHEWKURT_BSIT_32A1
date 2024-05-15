d82266bd89e16d3de76b8a83cf9b1c8c52e4c55c
Arciaga-MatthewKurt-BSIT32E1 <mkurt.arciaga@gmail.com>
2024-05-16 01:31:53 +0800

    10 things about me...

--- PREFINALS_AUTH_ARCIAGA_MATTHEW/ProtectedApi/obj/Debug/net8.0/ProtectedApi.AssemblyInfo.cs
+++ PREFINALS_AUTH_ARCIAGA_MATTHEW/ProtectedApi/obj/Debug/net8.0/ProtectedApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProtectedApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6c519c13f25ece1b45362224cab6ff652dffaa8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40158799c9b5a72ae7fefc8b7d90f7ab8194e9e0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProtectedApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProtectedApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
