51038f3c1853c2250a47c3aba8b5e50b414e745e
Arciaga-MatthewKurt-BSIT32E1 <mkurt.arciaga@gmail.com>
2024-05-16 01:02:03 +0800

    AuthController Update

--- PREFINALS_AUTH_ARCIAGA_MATTHEW/AuthServer/obj/Debug/net8.0/AuthServer.AssemblyInfo.cs
+++ PREFINALS_AUTH_ARCIAGA_MATTHEW/AuthServer/obj/Debug/net8.0/AuthServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ac32f5b43ff7cb19d7adaf1b90aaef372226a831")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6663b181083e787b1f18020d7b0b8db366311c4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
