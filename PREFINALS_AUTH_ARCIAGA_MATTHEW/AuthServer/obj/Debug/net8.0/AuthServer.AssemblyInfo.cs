29e0aa4e841222add8d0a13f6eb9c953991b38d9
Arciaga-MatthewKurt-BSIT32E1 <mkurt.arciaga@gmail.com>
2024-05-16 01:05:44 +0800

    Jwt Settings

--- PREFINALS_AUTH_ARCIAGA_MATTHEW/AuthServer/obj/Debug/net8.0/AuthServer.AssemblyInfo.cs
+++ PREFINALS_AUTH_ARCIAGA_MATTHEW/AuthServer/obj/Debug/net8.0/AuthServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+abf45fab856330b0a9d7e25aee8af9dba12da15c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45b3b819f970af1f364f41c4c97d53b1d56d2bfa")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuthServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
